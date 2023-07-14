331afd857d6af7529f48bf4a96e9562a957bea59
Rui Santos <randomnerdtutorials@gmail.com>
2023-07-14 15:10:56 +0100

    Update main.cpp

--- Module_3_Web_Servers/2_Outputs/2_1_Output/src/main.cpp
+++ Module_3_Web_Servers/2_Outputs/2_1_Output/src/main.cpp
@@ -27,10 +27,12 @@ String ledState;
 
 // Initialize SPIFFS
 void initSPIFFS() {
-  if (!SPIFFS.begin()) {
+  if (!SPIFFS.begin(true)) {
     Serial.println("An error has occurred while mounting SPIFFS");
   }
-  Serial.println("SPIFFS mounted successfully");
+  else {
+    Serial.println("SPIFFS mounted successfully");
+  }
 }
 
 // Initialize WiFi
