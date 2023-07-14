cb93c7e3115c743995988ada9ca2e59388da13e8
Rui Santos <randomnerdtutorials@gmail.com>
2023-07-14 15:11:53 +0100

    Update main.cpp

--- Module_3_Web_Servers/2_Outputs/2_5_Multiple_Outputs_WebSocket/src/main.cpp
+++ Module_3_Web_Servers/2_Outputs/2_5_Multiple_Outputs_WebSocket/src/main.cpp
@@ -35,9 +35,10 @@ void initSPIFFS() {
   if (!SPIFFS.begin(true)) {
     Serial.println("An error has occurred while mounting SPIFFS");
   }
-  Serial.println("SPIFFS mounted successfully");
+  else {
+    Serial.println("SPIFFS mounted successfully");
+  }
 }
-
 // Initialize WiFi
 void initWiFi() {
   WiFi.mode(WIFI_STA);
