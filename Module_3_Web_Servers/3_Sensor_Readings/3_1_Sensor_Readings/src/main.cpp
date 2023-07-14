1d002e178ca0354d809e983b9dd5ef990cf09db0
Rui Santos <randomnerdtutorials@gmail.com>
2023-07-14 15:12:44 +0100

    Update main.cpp

--- Module_3_Web_Servers/3_Sensor_Readings/3_1_Sensor_Readings/src/main.cpp
+++ Module_3_Web_Servers/3_Sensor_Readings/3_1_Sensor_Readings/src/main.cpp
@@ -57,7 +57,9 @@ void initSPIFFS() {
   if (!SPIFFS.begin(true)) {
     Serial.println("An error has occurred while mounting SPIFFS");
   }
-  Serial.println("SPIFFS mounted successfully");
+  else {
+    Serial.println("SPIFFS mounted successfully");
+  }
 }
 
 // Initialize WiFi
