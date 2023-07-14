b929aa13199d3a323d29b3319a9022db7892b55f
Rui Santos <randomnerdtutorials@gmail.com>
2023-07-14 15:23:48 +0100

    Update main.cpp

--- Module_3_Web_Servers/2_Outputs/2_3_Outputs _Websockets_ESP8266/src/main.cpp
+++ Module_3_Web_Servers/2_Outputs/2_3_Outputs _Websockets_ESP8266/src/main.cpp
@@ -32,7 +32,9 @@ void initFS() {
   if (!LittleFS.begin()) {
     Serial.println("An error has occurred while mounting LittleFS");
   }
-  Serial.println("LittleFS mounted successfully");
+  else {
+    Serial.println("LittleFS mounted successfully");
+  }
 }
 
 // Initialize WiFi
